e20b5f12d81475b5e75caa468df78ef5e7fea524
dani <dani.zaghian@gmail.com>
2020-02-12 11:47:04 -0800

    adds solutions to exercises

--- practice.js
+++ practice.js
@@ -151,46 +151,207 @@
 
 
 
-// Write a function that accepts an array of strings and returns a new array containing every other string from the original array. For example, if the input is ["a", "b", "c", "d", "e", "f"], the output should be ["a", "c", "e"].
-function selectEvenItems(strings) {
-  var result = [];
+// // Write a function that accepts an array of strings and returns a new array containing every other string from the original array. For example, if the input is ["a", "b", "c", "d", "e", "f"], the output should be ["a", "c", "e"].
+// function selectEvenItems(strings) {
+//   var result = [];
+//   var index = 0;
+//   strings.forEach(function(string) {
+//     if (index % 2 === 0) {
+//       result.push(string);
+//     }
+//     index++;
+//   });
+//   return result;
+// }
+
+// console.log(selectEvenItems(["a", "b", "c", "d", "e", "f"]));
+
+
+
+// // Write a method that accepts one argument - an array of numbers. The method should return the greatest number. For example, if the input is [5, 4, 8, 1, 2], the output should be 8.
+// function max(numbers) {
+//   var currentMax = numbers[0];
+//   numbers.forEach(function(number) {
+//     if (number > currentMax) {
+//       currentMax = number;
+//     }
+//   });
+//   return currentMax;
+// }
+
+// console.log(max([5, 4, 8, 1, 2]));
+
+
+// // Write a method that accepts one argument - an array of numbers that are in ascending order. The method that returns a new array with the same values in descending order. However, do not use the "reverse" method built in to Ruby.
+// function descending(numbers) {
+//   var result = [];
+//   var index = numbers.length - 1;
+//   for (var i = 0; i < numbers.length; i++) {
+//     result.push(numbers[index]);
+//     index--;
+//   }
+//   return result;
+// }
+
+// console.log(descending([1, 3, 5, 7]));
+
+
+
+
+
+// 1. Write a function that takes in an array of numbers and returns its sum.
+function sum(numbers) {
+  var result = 0;
   var index = 0;
-  strings.forEach(function(string) {
-    if (index % 2 === 0) {
-      result.push(string);
-    }
-    index++;
-  });
-  return result;
-}
-
-console.log(selectEvenItems(["a", "b", "c", "d", "e", "f"]));
-
-
-
-// Write a method that accepts one argument - an array of numbers. The method should return the greatest number. For example, if the input is [5, 4, 8, 1, 2], the output should be 8.
-function max(numbers) {
-  var currentMax = numbers[0];
-  numbers.forEach(function(number) {
-    if (number > currentMax) {
-      currentMax = number;
-    }
-  });
-  return currentMax;
-}
-
-console.log(max([5, 4, 8, 1, 2]));
-
-
-// Write a method that accepts one argument - an array of numbers that are in ascending order. The method that returns a new array with the same values in descending order. However, do not use the "reverse" method built in to Ruby.
-function descending(numbers) {
-  var result = [];
-  var index = numbers.length - 1;
-  for (var i = 0; i < numbers.length; i++) {
-    result.push(numbers[index]);
-    index--;
+  while (index < numbers.length) {
+    result += numbers[index];
+    index += 1;
   }
   return result;
 }
 
-console.log(descending([1, 3, 5, 7]));
+console.log(sum([1, 2, 3]));
+
+// 2. Write a function that takes in an array of strings and returns the smallest string.
+function smallestString(strings) {
+  var result = strings[0];
+  var index = 0;
+  while (index < strings.length) {
+    if (strings[index].length < result.length) {
+      result = strings[index];
+    }
+    index += 1;
+  }
+  return result;
+}
+
+console.log(smallestString(["apple", "orange", "grapes"]));
+
+// 3. Write a function that takes in an array of numbers and returns a new array with the numbers in reverse order.
+function reversedNumbers(numbers) {
+  var result = [];
+  var index = numbers.length - 1;
+  while (index >= 0) {
+    result.push(numbers[index]);
+    index -= 1;
+  }
+  return result;
+}
+
+console.log(reversedNumbers([1, 2, 3]));
+
+// 4. Write a function that takes in an array of words and returns the number of words that begin with the letter "a".
+function aWords(words) {
+  var result = 0;
+  var index = 0;
+  while (index < words.length) {
+    if (words[index][0] === "a") {
+      result += 1;
+    }
+    index += 1;
+  }
+  return result;
+}
+
+console.log(aWords(["apple", "orange", "grapes"]));
+
+// 5. Write a function that takes in an array of strings and joins them together to make a single string separated by commas.
+function joinWithCommas(strings) {
+  var result = "";
+  var index = 0;
+  while (index < strings.length) {
+    result += strings[index] + ",";
+    index += 1;
+  }
+  return result;
+}
+
+console.log(joinWithCommas(["apple", "orange", "grapes"]));
+
+// 6. Write a function that takes in an array of numbers and returns the product of all the numbers (each number multiplied by each other).
+function product(numbers) {
+  var result = 1;
+  var index = 0;
+  while (index < numbers.length) {
+    result = result * numbers[index];
+    index += 1;
+  }
+  return result;
+}
+
+console.log(product([1, 2, 3, 4]));
+
+// 7. Write a function that takes in an array of numbers and returns the two smallest numbers.
+function twoSmallestNumbers(numbers) {
+  var smallestIndex = 0;
+  var smallest = numbers[smallestIndex];
+  var index = 0;
+  while (index < numbers.length) {
+    if (numbers[index] < smallest) {
+      smallest = numbers[index];
+      smallestIndex = index;
+    }
+    index += 1;
+  }
+  var secondSmallest = numbers[smallestIndex + 1];
+  index = 0;
+  while (index < numbers.length) {
+    if (numbers[index] < secondSmallest && index !== smallestIndex) {
+      secondSmallest = numbers[index];
+    }
+    index += 1;
+  }
+  return [smallest, secondSmallest];
+}
+
+console.log(twoSmallestNumbers([1, 10, 9, 3, 23, 12]));
+
+// 8. Write a function that takes in an array of numbers and returns a count of how many zeros are in the array.
+function zeroCount(numbers) {
+  var result = 0;
+  var index = 0;
+  while (index < numbers.length) {
+    if (numbers[index] === 0) {
+      result += 1;
+    }
+    index += 1;
+  }
+  return result;
+}
+
+console.log(zeroCount([9, 32, 0, 2, 0, 12, 5, 2, 0, 1]));
+
+// 9. Write a function that takes in an array of numbers and returns true if all the numbers are bigger than 10, otherwise returns false.
+function isAllBig(numbers) {
+  var result = true;
+  var index = 0;
+  while (index < numbers.length) {
+    if (numbers[index] <= 10) {
+      result = false;
+    }
+    index += 1;
+  }
+  return result;
+}
+
+console.log(isAllBig([11, 23, 32]));
+
+// 10. Write a function that takes in an array of words and returns the number of times the letter "a" appeared in total.
+function aCount(words) {
+  var result = 0;
+  var index = 0;
+  while (index < words.length) {
+    var word = words[index];
+    var index2 = 0;
+    while (index2 < word.length) {
+      if (word[index2] === "a") {
+        result += 1;
+      }
+      index2 += 1;
+    }
+    index += 1;
+  }
+  return result;
+}
+
+console.log(aCount(["apple", "orange", "grapes"]));
--- practice.rb
+++ practice.rb
@@ -151,44 +151,204 @@
 # print_sums([1, 5, 10], [100, 500, 1000])
 
 
-# Write a function that accepts an array of strings and returns a new array containing every other string from the original array. For example, if the input is ["a", "b", "c", "d", "e", "f"], the output should be ["a", "c", "e"].
-def select_even_items(strings)
-  result = []
+# # Write a function that accepts an array of strings and returns a new array containing every other string from the original array. For example, if the input is ["a", "b", "c", "d", "e", "f"], the output should be ["a", "c", "e"].
+# def select_even_items(strings)
+#   result = []
+#   index = 0
+#   strings.each do |string|
+#     if index % 2 == 0
+#       result << string
+#     end
+#     index = index + 1
+#   end
+#   result
+# end
+# p select_even_items(["a", "b", "c", "d", "e", "f"])
+
+
+
+# # Write a method that accepts one argument - an array of numbers. The method should return the greatest number. For example, if the input is [5, 4, 8, 1, 2], the output should be 8.
+# def max(numbers)
+#   current_max = numbers[0]
+#   numbers.each do |number|
+#     if number > current_max
+#       current_max = number
+#     end
+#   end
+#   current_max
+# end
+# p max([5, 4, 8, 1, 2])
+
+
+
+# # Write a method that accepts one argument - an array of numbers that are in ascending order. The method that returns a new array with the same values in descending order. However, do not use the "reverse" method built in to Ruby.
+# def descending(numbers)
+#   result = []
+#   index = numbers.length - 1
+#   numbers.length.times do
+#     result << numbers[index]
+#     index = index - 1
+#   end
+#   result
+# end
+# p descending([1, 3, 5, 7])
+
+
+
+
+# 1. Write a function that takes in an array of numbers and returns its sum.
+def sum(numbers)
+  result = 0
   index = 0
-  strings.each do |string|
-    if index % 2 == 0
-      result << string
-    end
-    index = index + 1
+  while index < numbers.length
+    result += numbers[index]
+    index += 1
   end
   result
 end
-p select_even_items(["a", "b", "c", "d", "e", "f"])
 
+p sum([1, 2, 3])
 
-
-# Write a method that accepts one argument - an array of numbers. The method should return the greatest number. For example, if the input is [5, 4, 8, 1, 2], the output should be 8.
-def max(numbers)
-  current_max = numbers[0]
-  numbers.each do |number|
-    if number > current_max
-      current_max = number
+# 2. Write a function that takes in an array of strings and returns the smallest string.
+def smallest_string(strings)
+  result = strings[0]
+  index = 0
+  while index < strings.length
+    if strings[index].length < result.length
+      result = strings[index]
     end
+    index += 1
   end
-  current_max
+  result
 end
-p max([5, 4, 8, 1, 2])
 
+p smallest_string(["apple", "orange", "grapes"])
 
-
-# Write a method that accepts one argument - an array of numbers that are in ascending order. The method that returns a new array with the same values in descending order. However, do not use the "reverse" method built in to Ruby.
-def descending(numbers)
+# 3. Write a function that takes in an array of numbers and returns a new array with the numbers in reverse order.
+def reversed_numbers(numbers)
   result = []
   index = numbers.length - 1
-  numbers.length.times do
+  while index >= 0
     result << numbers[index]
-    index = index - 1
+    index -= 1
   end
   result
 end
-p descending([1, 3, 5, 7])
+
+p reversed_numbers([1, 2, 3])
+
+# 4. Write a function that takes in an array of words and returns the number of words that begin with the letter "a".
+def a_words(words)
+  result = 0
+  index = 0
+  while index < words.length
+    if words[index][0] == "a"
+      result += 1
+    end
+    index += 1
+  end
+  result
+end
+
+p a_words(["apple", "orange", "grapes"])
+
+# 5. Write a function that takes in an array of strings and joins them together to make a single string separated by commas.
+def join_with_commas(strings)
+  result = ""
+  index = 0
+  while index < strings.length
+    result += strings[index] + ","
+    index += 1
+  end
+  result
+end
+
+p join_with_commas(["apple", "orange", "grapes"])
+
+# 6. Write a function that takes in an array of numbers and returns the product of all the numbers (each number multiplied by each other).
+def product(numbers)
+  result = 1
+  index = 0
+  while index < numbers.length
+    result = result * numbers[index]
+    index += 1
+  end
+  result
+end
+
+p product([1, 2, 3, 4])
+
+# 7. Write a function that takes in an array of numbers and returns the two smallest numbers.
+def two_smallest_numbers(numbers)
+  smallest_index = 0
+  smallest = numbers[smallest_index]
+  index = 0
+  while index < numbers.length
+    if numbers[index] < smallest
+      smallest = numbers[index]
+      smallest_index = index
+    end
+    index += 1
+  end
+  second_smallest = numbers[smallest_index - 1]
+  index = 0
+  while index < numbers.length
+    if numbers[index] < second_smallest && index != smallest_index
+      second_smallest = numbers[index]
+    end
+    index += 1
+  end
+  [smallest, second_smallest]
+end
+
+p two_smallest_numbers([1, 10, 9, 3, 23, 12])
+
+# 8. Write a function that takes in an array of numbers and returns a count of how many zeros are in the array.
+def zero_count(numbers)
+  result = 0
+  index = 0
+  while index < numbers.length
+    if numbers[index] == 0
+      result += 1
+    end
+    index += 1
+  end
+  result
+end
+
+p zero_count([9, 32, 0, 2, 0, 12, 5, 2, 0, 1])
+
+# 9. Write a function that takes in an array of numbers and returns true if all the numbers are bigger than 10, otherwise returns false.
+def is_all_big(numbers)
+  result = true
+  index = 0
+  while index < numbers.length
+    if numbers[index] <= 10
+      result = false
+    end
+    index += 1
+  end
+  result
+end
+
+p is_all_big([11, 23, 32])
+
+# 10. Write a function that takes in an array of words and returns the number of times the letter "a" appeared in total.
+def a_count(words)
+  result = 0
+  index = 0
+  while index < words.length
+    word = words[index]
+    index2 = 0
+    while index2 < word.length
+      if word[index2] == "a"
+        result += 1
+      end
+      index2 += 1
+    end
+    index += 1
+  end
+  result
+end
+
+p a_count(["apple", "orange", "grapes"])
